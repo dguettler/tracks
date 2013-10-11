904d4540197d404085f89d8d9d605c690b4f956d
Daniel Guettler <daniel.guettler@gmail.com>
2013-10-11 11:07:47 -0400

    - cleanup sql queries

--- app/models/tag_cloud.rb
+++ app/models/tag_cloud.rb
@@ -15,7 +15,8 @@ class TagCloud
   def compute
     levels=10
 
-    @tags = Tag.find_by_sql(sql).sort_by { |tag| tag.name.downcase }
+    params = [sql, user.id]
+    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
 
     max, @tags_min = 0, 0
     @tags.each { |t|
@@ -25,9 +26,8 @@ class TagCloud
 
     @tags_divisor = ((max - @tags_min) / levels) + 1
 
-    @tags_90days = Tag.find_by_sql(
-      [sql_90days, user.id, @cut_off, @cut_off]
-    ).sort_by { |tag| tag.name.downcase }
+    params = [sql(@cut_off), user.id, @cut_off, @cut_off]
+    @tags_90days = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
 
     max_90days, @tags_min_90days = 0, 0
     @tags_90days.each { |t|
@@ -39,27 +39,17 @@ class TagCloud
   end
 
   private
-    def sql
-      query = "SELECT tags.id, name, count(*) AS count"
-      query << " FROM taggings, tags, todos"
-      query << " WHERE tags.id = tag_id"
-      query << " AND taggings.taggable_id = todos.id"
-      query << " AND todos.user_id="+user.id.to_s+" "
-      query << " AND taggings.taggable_type='Todo' "
-      query << " GROUP BY tags.id, tags.name"
-      query << " ORDER BY count DESC, name"
-      query << " LIMIT 100"
-    end
-
-    def sql_90days
-      query = "SELECT tags.id, tags.name AS name, count(*) AS count"
+    def sql(cut_off = nil)
+      query = "SELECT tags.id, tags.name, count(*) AS count"
       query << " FROM taggings, tags, todos"
       query << " WHERE tags.id = tag_id"
       query << " AND todos.user_id=? "
-      query << " AND taggings.taggable_type='Todo' "
       query << " AND taggings.taggable_id=todos.id "
-      query << " AND (todos.created_at > ? OR "
-      query << "      todos.completed_at > ?) "
+      query << " AND taggings.taggable_type='Todo' "
+      if cut_off
+        query << " AND (todos.created_at > ? OR "
+        query << "      todos.completed_at > ?) "
+      end
       query << " GROUP BY tags.id, tags.name"
       query << " ORDER BY count DESC, name"
       query << " LIMIT 100"
