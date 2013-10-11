ccb148bbda88c0a8f358108c97645cba2156e8d9
Daniel Guettler <daniel.guettler@gmail.com>
2013-10-11 10:55:00 -0400

    - moved sql to private method

--- app/models/tag_cloud.rb
+++ app/models/tag_cloud.rb
@@ -34,19 +34,8 @@ class TagCloud
 
     @tags_divisor = ((max - @tags_min) / levels) + 1
 
-    query = "SELECT tags.id, tags.name AS name, count(*) AS count"
-    query << " FROM taggings, tags, todos"
-    query << " WHERE tags.id = tag_id"
-    query << " AND todos.user_id=? "
-    query << " AND taggings.taggable_type='Todo' "
-    query << " AND taggings.taggable_id=todos.id "
-    query << " AND (todos.created_at > ? OR "
-    query << "      todos.completed_at > ?) "
-    query << " GROUP BY tags.id, tags.name"
-    query << " ORDER BY count DESC, name"
-    query << " LIMIT 100"
     @tags_90days = Tag.find_by_sql(
-      [query, user.id, @cut_off, @cut_off]
+      [sql_90days, user.id, @cut_off, @cut_off]
     ).sort_by { |tag| tag.name.downcase }
 
     max_90days, @tags_min_90days = 0, 0
@@ -57,4 +46,19 @@ class TagCloud
 
     @tags_divisor_90days = ((max_90days - @tags_min_90days) / levels) + 1
   end
+
+  private
+    def sql_90days
+      query = "SELECT tags.id, tags.name AS name, count(*) AS count"
+      query << " FROM taggings, tags, todos"
+      query << " WHERE tags.id = tag_id"
+      query << " AND todos.user_id=? "
+      query << " AND taggings.taggable_type='Todo' "
+      query << " AND taggings.taggable_id=todos.id "
+      query << " AND (todos.created_at > ? OR "
+      query << "      todos.completed_at > ?) "
+      query << " GROUP BY tags.id, tags.name"
+      query << " ORDER BY count DESC, name"
+      query << " LIMIT 100"
+    end
 end
